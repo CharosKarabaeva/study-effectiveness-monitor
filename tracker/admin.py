a51e6f7dbbc772500e1e5ee24fafc1b4d21c780b
Чарос Карабаева <charos.karabaeva@gmail.com>
2026-01-19 11:41:47 +0500

    добавила админку для всех моделей tracker

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import StudyDay
+from .models import StudyDay, Recommendation, StudyMetric
 
 
 @admin.register(StudyDay)
@@ -11,6 +11,16 @@ class StudyDayAdmin(admin.ModelAdmin):
         'productivity',
         'created_at',
     )
-
     list_filter = ('date', 'productivity', 'mood')
     search_fields = ('comment',)
+
+
+@admin.register(Recommendation)
+class RecommendationAdmin(admin.ModelAdmin):
+    list_display = ('study_day', 'created_at')
+    search_fields = ('text',)
+
+
+@admin.register(StudyMetric)
+class StudyMetricAdmin(admin.ModelAdmin):
+    list_display = ('date', 'avg_mood', 'avg_fatigue', 'avg_productivity')
